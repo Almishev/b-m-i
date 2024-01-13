0a0bf948ed9a2a1d1ad0a0374d38f681c3bb7443
Almishev <antonalmishev@abv.bg>
2024-01-13 16:32:19 +0200

    Add GitHub Pages deploy script

--- src/Footer.js
+++ src/Footer.js
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import "./Footer.css"
+
+
 
 const Footer = () => {
+  
+
+  const [time, setTime] = useState(new Date());
+    
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        }
+    }, []);
+
+    function formatTime(){
+        let hours = time.getHours();
+        const minutes = time.getMinutes();
+        const seconds = time.getSeconds();
+        const meridiem = hours >= 12 ? "PM" : "AM";
+
+        hours = hours % 12 || 12;
+
+        return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`;
+    }
+    
+    function padZero(number){
+        return (number < 10 ? "0" : "") + number;
+    }
+
   return (
-    <footer style={footerStyle}>
-      <p>&copy; 2024 Anton Almishev</p>
+
+    <footer className="footer">
+      <div className="left-part">
+        <p>&copy; 2024 Anton Almishev</p>
+      </div>
+      <div className="right-part">
+        <p>{formatTime()}</p>
+      </div>
     </footer>
   );
 };
 
-const footerStyle = {
-  backgroundColor: '#333',
-  color: '#fff',
-  textAlign: 'center',
-  padding: '0.3rem',
-  position: 'fixed',
-  bottom: ' 0',
-  width: '100%',
- 
-};
 
 export default Footer;
